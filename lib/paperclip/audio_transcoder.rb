2913a1564851d10643a46e32146280ba6edf2f93
Nolan Lawson <nolan@nolanlawson.com>
2017-11-25 17:49:38 -0800

    Tweak audio encode params, increase limit to 5 minutes

--- lib/paperclip/audio_transcoder.rb
+++ lib/paperclip/audio_transcoder.rb
@@ -5,8 +5,8 @@ module Paperclip
     def make
       meta = ::Av.cli.identify(@file.path)
       # {:length=>"0:00:02.14", :duration=>2.14, :audio_encode=>"mp3", :audio_bitrate=>"44100 Hz", :audio_channels=>"mono"}
-      if meta[:duration] > 60.0
-        raise Mastodon::ValidationError, "Audio uploads must be less than 60 seconds in length."
+      if meta[:duration] > 300.0
+        raise Mastodon::ValidationError, "Audio uploads must be less than 5 minutes in length."
       end
       
       final_file = Paperclip::Transcoder.make(file, options, attachment)
